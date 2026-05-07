8cec1bc587667e9f0f1e842eff753bd0b17e23f8
rabiulislam5334 <rabiulislam5334@gmail.com>
2026-05-07 20:11:18 +0600

    type/index.ts file added

---
+++ app/types/index.ts
@@ -0,0 +1,41 @@
+export interface Surah {
+  id: number;
+  name: string;
+  transliteration: string;
+  translation: string;
+  type: "meccan" | "medinan";
+  total_verses: number;
+}
+
+export interface Ayah {
+  id: number;
+  surah: number;
+  verse: number;
+  text: string;
+  translation: string;
+  audio_url?: string;
+}
+
+export interface SurahWithAyahs {
+  surah: Surah;
+  ayahs: Ayah[];
+  total: number;
+}
+
+export interface SearchResult {
+  surah_id: number;
+  surah_name: string;
+  surah_transliteration: string;
+  verse: number;
+  text: string;
+  translation: string;
+  highlight: string;
+}
+
+export interface FontSettings {
+  arabicFont: "uthmani" | "amiri" | "scheherazade";
+  arabicFontSize: number;
+  translationFontSize: number;
+}
+
+export type ArabicFont = "uthmani" | "amiri" | "scheherazade";
